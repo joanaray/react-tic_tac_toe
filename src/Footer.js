4ee7d2979f7d52b1cfbb11ab5fe45c1b465ba640
Joana <account@joana.cc>
2024-04-17 17:40:45 +0100

    Open Graph props and repo link on the footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <p className="small"><a href="http://www.joana.cc">Joana Ray</a> &bull; 2024</p>
+        <p className="small"><a href="https://github.com/joanaray/react-tic_tac_toe">GitHub Repo</a> &bull; <a href="http://www.joana.cc">Joana Ray</a> &bull; 2024</p>
       </footer>
     </>
   );
